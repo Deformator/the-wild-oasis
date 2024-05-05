b15775c3d18374963dde133e09d63afa374bf83d
Deformator <dammandrii@gmail.com>
2024-05-05 13:42:07 -0600

    - Style components lib - Adding global styles - Button, Input components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-function App (){
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import  Button from './ui/Button';
+import  Input from './ui/Input';
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
+function App() {
   return (
-    <div>
-      Hello World
-    </div>
-  )
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert('Check in')}>Check in</Button>
+        <Button onClick={() => alert('Check out')}>Check out</Button>
+        <Input type="number" placeholder="Number of guests"></Input>
+      </StyledApp>
+    </>
+  );
 }
 
-export default App
+export default App;

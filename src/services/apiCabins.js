5a223a920f34c6afb931c14218d9264fa24b2922
Deformator <dammandrii@gmail.com>
2024-05-08 18:48:59 -0600

    - form errors - uploading images

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from './supabase';
+import supabase, { supabaseUrl } from './supabase';
 
 export async function getCabins() {
   const { data, error } = await supabase.from('cabins').select('*');
@@ -6,6 +6,40 @@ export async function getCabins() {
   if (error) {
     console.log(error);
     throw new Error('Cabins could not be loaded');
+  }
+
+  return data;
+}
+
+//1. Create a cabin
+export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    '/',
+    ''
+  );
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+  const { data, error } = await supabase
+    .from('cabins')
+    .insert([{ ...newCabin, image: imagePath }])
+    .select();
+
+  if (error) {
+    console.log(error);
+    throw new Error('Cabins could not be deleted');
+  }
+
+  //2. Upload image
+  const { error: storageError } = await supabase.storage
+    .from('cabin-images')
+    .upload(imageName, newCabin.image);
+
+  //3. Delete the cabin if error uploading image
+  if (storageError) {
+    await supabase.from('cabins').delete().eq('id', data.id);
+    console.log(storageError);
+    throw new Error(
+      'Cabins image could not be uploaded and the cabin was not created'
+    );
   }
 
   return data;
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://crggbookseynxxkvvbqv.supabase.co';
+export const supabaseUrl = 'https://crggbookseynxxkvvbqv.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyZ2dib29rc2V5bnh4a3Z2YnF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUxMjg4MzksImV4cCI6MjAzMDcwNDgzOX0.93mEga1UWP8hh2GULV01Z2rCv42t4_6tOHxWEXKSa-0';
 const supabase = createClient(supabaseUrl, supabaseKey);

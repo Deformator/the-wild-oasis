675fef238df86691af28e2bb9871228830464727
Deformator <dammandrii@gmail.com>
2024-05-13 13:34:10 -0600

    - booking table filtering - booking table sorting

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,15 +1,30 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { getBookings } from '../../services/apiBookings.js';
+import { useSearchParams } from 'react-router-dom';
 
 export function useBookings() {
+  const [serachParams] = useSearchParams();
+
+  // FILTER
+  const filterValue = serachParams.get('status');
+  const filter =
+    !filterValue || filterValue === 'all'
+      ? null
+      : { field: 'status', value: filterValue };
+
+  // SORT
+  const sortByRaw = serachParams.get('sortBy') || 'startDate-desc';
+  const [field, direction] = sortByRaw.split('-');
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['bookings'],
-    queryFn: getBookings,
+    queryKey: ['bookings', filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
   return { isLoading, error, bookings };
 }
